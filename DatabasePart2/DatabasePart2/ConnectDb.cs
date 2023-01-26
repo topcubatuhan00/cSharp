932c7e469fb0566db64d8c239070438757dacc6c
sadecebatu0 <topcubatuhan00@gmail.com>
2023-01-26 15:40:22 +0300

    database crud - stroed procedure

---
+++ DatabasePart2/DatabasePart2/ConnectDb.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Collections;
+using System.Data;
+
+namespace DatabasePart2
+{
+    internal class ConnectDb
+    {
+        private string BaglantiAdresi = "Data Source=batu\\SQLEXPRESS;Initial Catalog=test;Integrated Security=True";
+        SqlConnection con = null;
+        SqlDataReader dr = null;
+        SqlCommand cmd = null;
+        public ConnectDb() {
+            con = new SqlConnection(BaglantiAdresi);
+            cmd = new SqlCommand();
+
+        }
+
+        public bool insertClassData(String className, int capacity)
+        {
+            bool cond = false;
+
+            cmd.Connection = con;
+            cmd.CommandText = "INSERT INTO SINIF VALUES (@sınıfad, @kapasite)";
+            cmd.Parameters.AddWithValue("@sınıfad", className);
+            cmd.Parameters.AddWithValue("@kapasite", capacity);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            cond = true;
+            con.Close();
+
+            return cond;
+        }
+        public void getData()
+        {
+            
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT * FROM sınıf";
+            con.Open();
+            dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Console.Write(dr["sınıfAd"]);
+            }
+
+            dr.Close();
+            con.Close();
+        }
+        public bool deleteClassData(int id)
+        {
+            bool cond = false;
+
+            cmd.Connection = con;
+            cmd.CommandText = "DELETE FROM SINIF WHERE id=@id";
+            cmd.Parameters.AddWithValue("@id", id);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            cond = true;
+            con.Close();
+
+            return cond;
+        }
+        public bool updateClassData(int id, int capacity)
+        {
+            bool cond = false;
+
+            cmd.Connection = con;
+            cmd.CommandText = "UPDATE SINIF SET kapasite=@kap where id=@id";
+            cmd.Parameters.AddWithValue("@kap", capacity);
+            cmd.Parameters.AddWithValue("@id", id);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            cond = true;
+            con.Close();
+
+            return cond;
+        }
+
+        public void runProcedure()
+        {
+            con.Open();
+            cmd = new SqlCommand("sp_Kapasite", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            SqlDataReader rdr = cmd.ExecuteReader();
+            if(rdr.Read())
+                Console.WriteLine("kapasite : " + rdr["kapasite"]);
+           
+        }
+    }
+}
---
+++ DatabasePart2/DatabasePart2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabasePart2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ConnectDb connectDb = new ConnectDb();
+            /*connectDb.getData();
+
+            if (connectDb.updateClassData(10,20))
+                Console.WriteLine("güncelleme başarılı");
+
+            if (connectDb.insertClassData("testData", 300))
+                Console.WriteLine("ekleme başarılı");
+
+            if (connectDb.deleteClassData(50))
+                Console.WriteLine("silme başarılı");
+
+            connectDb.getData();*/
+
+            connectDb.runProcedure();
+
+        }
+    }
+}
